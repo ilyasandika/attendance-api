d16eece5777a8fce2f7128735ad2ba0798e563c2
Ilyas Andika <andikailyas01@gmail.com>
2025-02-20 22:32:33 +0700

    feat: add locations, shifts, shift_days, schedules and attendances migrate

---
+++ database/migrations/2025_02_20_144125_create_shift_days_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shift_days', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('shift_id')->constrained("shifts")->onDelete('cascade');
+            $table->enum('name', ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']);
+            $table->time('check_in')->nullable();
+            $table->time('check_out')->nullable();
+            $table->time('break_start')->nullable();
+            $table->time('break_end')->nullable();
+            $table->boolean('is_off')->default(false);
+
+            $table->unique(['shift_id', 'name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shift_days');
+    }
+};
---
+++ database/migrations/2025_02_20_153045_create_attendances_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('schedule_id')->constrained('schedules')->onDelete('cascade');
+            $table->timestamp('date');
+            $table->timestamp('check_in_time');
+            $table->timestamp('check_out_time')->nullable();
+            $table->string('photo_check_in');
+            $table->string('photo_check_out')->nullable();
+            $table->decimal('latitude_check_in', 10, 8);
+            $table->decimal('longitude_check_in', 11, 8);
+            $table->decimal('latitude_check_out', 10, 8)->nullable();
+            $table->decimal('longitude_check_out', 11, 8)->nullable();
+            $table->enum('status', ['On Time', 'Late', 'Early Leave', 'Absent']);
+            $table->boolean('checkin_outside_location')->default(false);
+            $table->boolean('checkout_outside_location')->default(false);
+            $table->unique(['user_id', 'date']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
