c79eb9680c4a7563fb7908e7ca4a4f34db4a0413
Ilyas Andika <andikailyas01@gmail.com>
2025-02-21 14:03:47 +0700

    fix: change method to get user by id

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -41,36 +41,38 @@ class UserService
 
     public function findById(int $id)
     {
-        try {
-            $user = User::with('schedule.shift', 'schedule.location', 'profile.role', 'profile.department')->findOrFail($id);
-            $data = [
-                "id" => $user->id,
-                "employeeId" => $user->employee_id,
-                "employeeEmail" => $user->email,
-                "employeeName" => $user->profile->name,
-                "employeeGender" => $user->profile->gender,
-                "employeeBirthDate" => strtotime($user->profile->birth_date),
-                "employeePhoneNumber" => $user->profile->phone_number,
-                "employeeRole" => $user->profile->role->name,
-                "employeeDepartment" => $user->profile->department->name,
-                "employeeShift" => $user->schedule->shift->name,
-                "employeeWorkLocation" => $user->schedule->location->name,
-                "employeeWhatsApp" => $user->profile->whatsapp,
-                "employeeLinkedin" => $user->profile->linkedin,
-                "employeeTelegram" => $user->profile->telegram,
-                "employeeBiography" => $user->profile->Biography,
-                "accountStatus" => $user->status,
-            ];
+        $user = User::with('schedule.shift', 'schedule.location', 'profile.role', 'profile.department')->find($id);
 
-            return [
-                'status' => true,
-                'data' => $data
-            ];
-        } catch (\Exception $e) {
+        if (!$user) {
             return [
                 'status' => false,
                 'errors' => ['message' => "user not found"]
             ];
         }
+
+        $data = [
+            "id" => $user->id,
+            "employeeId" => $user->employee_id,
+            "employeeEmail" => $user->email,
+            "employeeName" => $user->profile->name,
+            "employeeGender" => $user->profile->gender,
+            "employeeBirthDate" => strtotime($user->profile->birth_date),
+            "employeePhoneNumber" => $user->profile->phone_number,
+            "employeeRole" => $user->profile->role->name,
+            "employeeDepartment" => $user->profile->department->name,
+            "employeeShift" => $user->schedule->shift->name,
+            "employeeWorkLocation" => $user->schedule->location->name,
+            "employeeWhatsApp" => $user->profile->whatsapp,
+            "employeeLinkedin" => $user->profile->linkedin,
+            "employeeTelegram" => $user->profile->telegram,
+            "employeeBiography" => $user->profile->Biography,
+            "accountStatus" => $user->status,
+        ];
+
+
+        return [
+            'status' => true,
+            'data' => $data
+        ];
     }
 }
